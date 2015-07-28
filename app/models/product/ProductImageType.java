031d8962505f223e568d52ae6c468c77597a2786
yangshanchang <369361116@qq.com>
2015-07-28 11:53:01 +0800

    增加 productImageType

---
+++ app/models/product/ProductImageType.java
@@ -0,0 +1,120 @@
+package models.product;
+
+/**
+ * Created by upshan on 15/7/24.
+ */
+
+import jodd.bean.BeanCopy;
+import models.constants.DeletedStatus;
+import net.sf.oval.constraint.MaxLength;
+import net.sf.oval.constraint.MinLength;
+import play.Logger;
+import play.data.validation.Min;
+import play.data.validation.Required;
+import play.db.jpa.Model;
+import play.modules.paginate.JPAExtPaginator;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 品牌
+ */
+@Entity
+@Table(name = "product_image_types")
+public class ProductImageType extends Model{
+
+    /**
+     * 图片类型名称
+     */
+    @Required(message = "图片类型名称不能为空.")
+    @MinLength(value = 2 , message = "图片类型名称不能低于2个字符")
+    @MaxLength(value = 20 , message = "图片类型名称不能大于20个字符")
+    @Column(name = "name")
+    public String name;
+
+    /**
+     * 图片宽度
+     */
+    @Required (message = "图片宽度不能为空")
+    @Min(value = 50 , message = "图片宽度不能低于50")
+    @Max(value = 1024 , message = "图片宽度不能大于1024")
+    @Column(name = "width")
+    public Integer width;
+
+
+    /**
+     * 图片高度
+     */
+    @Required(message = "图片高度不能为空")
+    @Min(value = 50 , message = "图片高度不能低于50")
+    @Max(value = 1024 , message = "图片高度不能大于1024")
+    @Column(name = "height")
+    public Integer height;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "created_at")
+    public Date createdAt;
+
+    /**
+     * 逻辑删除,0:未删除，1:已删除
+     */
+    @Enumerated(EnumType.ORDINAL)
+    public DeletedStatus deleted;
+
+
+    public static void update(Long id , ProductImageType newObject) {
+        ProductImageType oldBrand = ProductImageType.findById(id);
+        BeanCopy.beans(newObject, oldBrand).ignoreNulls(true).copy();
+        oldBrand.save();
+    }
+
+    /**
+     * 分页查询.
+     */
+    public static JPAExtPaginator<ProductImageType> findByCondition(Map<String, Object> conditionMap, String orderByExpress, int pageNumber, int pageSize) {
+        StringBuilder xsqlBuilder = new StringBuilder(" t.deleted=models.constants.DeletedStatus.UN_DELETED ")
+                .append("/~ and t.id = {id} ~/")
+                .append("/~ and t.name = {name} ~/")
+                .append("/~ and t.name like {searchName} ~/")
+                .append("/~ and t.brand.id = {brandId} ~/")
+                .append("/~ and t.createdAt = {createdAt} ~/");
+
+        if(conditionMap.get("isTop") != null) {
+            xsqlBuilder.append(" and t.brand = null ");
+            conditionMap.remove("isTop");
+        }
+
+        util.xsql.XsqlBuilder.XsqlFilterResult result = new util.xsql.XsqlBuilder().generateHql(xsqlBuilder.toString(), conditionMap);
+        JPAExtPaginator<ProductImageType> resultPage = new JPAExtPaginator<ProductImageType>("Brand t", "t", ProductImageType.class,
+                result.getXsql(), conditionMap).orderBy(orderByExpress);
+        Logger.info("brand Select SQL :" + result.getXsql() + "---");
+        resultPage.setPageNumber(pageNumber);
+        resultPage.setPageSize(pageSize);
+        resultPage.setBoundaryControlsEnabled(false);
+        return resultPage;
+    }
+
+    /**
+     * 查找第一类商品类别
+     * @return
+     */
+    public static List<ProductImageType> findTopBrand() {
+        return ProductImageType.find("deleted = ? and brand = null", DeletedStatus.UN_DELETED).fetch();
+    }
+
+
+    /**
+     * 根据父类查询
+     * @return
+     */
+    public static List<ProductImageType> findByBrand(ProductImageType brand) {
+        return ProductImageType.find("deleted = ? and brand = ?", DeletedStatus.UN_DELETED, brand).fetch();
+    }
+
+}
