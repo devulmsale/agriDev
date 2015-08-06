c34aba4d624237802c4037a629ec752c9941ecd0
weixiao <weixiao930101@163.com>
2015-08-06 10:18:04 +0800

    用户信息

--- app/controllers/weixin/Application.java
+++ app/controllers/weixin/Application.java
@@ -1,6 +1,7 @@
 package controllers.weixin;
 
 import controllers.auth.WxMpAuth;
+import controllers.weixin.auth.UserSecure;
 import models.base.WeixinUser;
 import play.mvc.Controller;
 import play.mvc.With;
@@ -8,11 +9,11 @@ import play.mvc.With;
 /**
  * Created by upshan on 15/8/5.
  */
-@With(WxMpAuth.class)
+@With(UserSecure.class)
 public class Application extends Controller {
 
     public static void index() {
-        WeixinUser wxUser = WxMpAuth.currentUser();
-        render(wxUser);
+
+       // render(wxUser);
     }
 }
