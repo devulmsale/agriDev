f78787739b5817d9ee0e39dff492eb9f54d00b7e
upshan <369361116@qq.com>
2015-08-12 16:08:20 +0800

    解决 商品数量为0 的时候 依然加载商品依然存在的问题

--- public/weixin/js/jquery.spinner.js
+++ public/weixin/js/jquery.spinner.js
@@ -7,7 +7,6 @@
       var container = $('<div></div>')
       container.addClass('spinner')
       var obj = $(this);
-
       var textField = $(this).addClass('value').attr('maxlength', '2').attr('id','sumCount').val(options.value)
         .bind('keyup paste change', function (e) {
           var field = $(this)
@@ -34,76 +33,16 @@
       function validateAndTrigger(field) {
         clearTimeout(container.data('timeout'))
         var value = validate(field);
-        var price = $('#price').val();
         var product_id = obj.attr("product_id");
         var product_type = obj.attr("product_type");
-        var productIds = $('#productIds').val();
-        var typeCount = $('#productType_'+product_type).html();
-        var isHave = false;
-        if(productIds != null && productIds.indexOf(",") >= 0) {
-          var ids = productIds.split(",");
-          for (i=0;i<ids.length ;i++ )   {
-            if(ids[i] == product_id) {
-              isHave = true;
-            }
-          }
-        }
-        if(value == 0) {
-          var newIds = "";
-          var ids = productIds.split(",");
-          for (i=0;i<ids.length ;i++ )   {
-            if(ids[i] != '' && ids[i] != product_id) {
-              newIds += ","+ids[i];
-            }
-          }
-          $('#productIds').val(newIds);
-          $('#productType_'+product_type).html(parseInt(typeCount) - 1)
-        }
-
-        var ids_count = $('#product_id_value').val();
-        var newIds_count = "";
-        //  4_5,5_2,6_1
-        if(ids_count != null && ids_count != '' && ids_count.indexOf(",") >= 0) {
-          var isHave2 = false;
-          var ids_countArray = ids_count.split(",");
-          for(var k = 0 ; k < ids_countArray.length ; k++) {
-            var id_count = ids_countArray[k];
-            if(id_count != null && id_count != '' && id_count.indexOf("_")) {
-              var pId = id_count.split("_")[0];
-              if(pId == product_id) {
-                if(value > 0) {
-                  newIds_count += pId+"_"+value + ",";
-                  isHave2 = true;
-                }
-              } else {
-                newIds_count += id_count + ",";
-              }
-            }
-          }
-          if(!isHave2) {
-            newIds_count += product_id+"_"+value;
-          }
-          $('#product_id_value').val(newIds_count);
-        } else {
-          var id_value = product_id+"_"+value+",";
-          $('#product_id_value').val(id_value);
-        }
-
-
-        if(!isHave) {
-          $('#productIds').val(productIds + "," + product_id);
-
-          $('#productType_'+product_type).html(parseInt(typeCount) + 1)
-        }
-        alert($('#product_id_value').val());
-        var carts=$('#product_id_value').val();
-        document.getElementById("carts").value=carts;
-        $('#sumPrice').html(value * price);
+        privateOperation(value , product_id , product_type);
+        //var price = $('#price').val();
+        //$('#sumPrice').html(value * price);
         if (!isInvalid(value)) {
           textField.trigger('update', [field, value])
         }
-        var sumcount=$('#sumCount').val();
-        document.getElementById("number").value=sumcount;
+        //var sumcount=$('#sumCount').val();
+        //document.getElementById("number").value=sumcount;
       }
 
       function validate(field) {
