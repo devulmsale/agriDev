6dd445a5bed0deac91723888352a07a412d078ba
gaoxingfei <gaoxingfei@yeah.net>
2015-08-20 17:08:28 +0800

    个人中心和并代码

--- app/controllers/weixin/userCenter/OrderController.java
+++ app/controllers/weixin/userCenter/OrderController.java
@@ -1,15 +1,19 @@
 package controllers.weixin.userCenter;
 
+import controllers.auth.WxMpAuth;
+import models.base.WeixinUser;
 import models.order.Order;
 import models.order.User;
 import play.Logger;
 import play.mvc.Controller;
+import play.mvc.With;
 
 import java.util.List;
 
 /**
  * Created by Administrator on 2015/8/19.
  */
+@With(WxMpAuth.class)
 public class OrderController extends Controller {
 
     /**
@@ -18,9 +22,9 @@ public class OrderController extends Controller {
      */
     public static void index(String type) {
         //TODO 暂时取到userId
-        User user=User.findById(2l);
-        List<Order> orderUnpaidList=Order.getUserUnPayOrders(user);
-        List<Order> orderPaidList=Order.getUserPaidOrders(user);
+        WeixinUser wxUser = WxMpAuth.currentUser();
+        List<Order> orderUnpaidList=Order.getUserUnPayOrders(wxUser.user);
+        List<Order> orderPaidList=Order.getUserPaidOrders(wxUser.user);
 
 
         if(type!=null&&type.equals("delete")){
