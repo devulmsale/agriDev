3f6f1385f8dd0480a0d7fbea3907de1997d98671
weixiao <weixiao930101@163.com>
2015-08-20 15:55:16 +0800

    modify index.html

--- app/controllers/weixin/Application.java
+++ app/controllers/weixin/Application.java
@@ -34,12 +34,14 @@ import java.util.*;
 /**
  * Created by upshan on 15/8/5.
  */
-//@With(WxMpAuth.class)
+@With(WxMpAuth.class)
 public class Application extends Controller {
 
     public static void index() {
+        //获取商户信息 TODO merchant_id  WxMpAuth.currentUser().merchant
+        Merchant merchant=Merchant.findById(21l);
         List<CouponBatch> couponBatchList = CouponBatch.findAll();
-        render(couponBatchList);
+        render(couponBatchList , merchant);
     }
 
 
